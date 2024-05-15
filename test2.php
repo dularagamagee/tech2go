b0dbf00b292b04cb1bca07e54db62721c574690a
DarkDuli <103023323+DarkDuli@users.noreply.github.com>
2024-05-15 23:41:15 +0200

    Update test2.php

--- test2.php
+++ test2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to my website</title>
+    <title>AWS DON</title>
     <link rel="stylesheet" href="style2.css">
 </head>
 <body>
@@ -11,7 +11,8 @@
         <img src="https://i.imgur.com/gIWOMuW.jpeg" width="3840" height="2160" alt="background">
     </picture>
     <figure>
-        <h1>Welcome to my website</h1>
+        <h1>AWS DON</h1>
+        <p1>Premessa</p1>
         <figcaption>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         </figcaption>
